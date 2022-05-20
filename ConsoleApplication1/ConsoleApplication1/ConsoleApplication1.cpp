1c6d514f125599e776fa9b3efd3156fee4da93a0
tusugit <1471205281@qq.com>
2022-05-20 18:10:09 +0800

    update

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,21 +3,27 @@
 #include <iostream>
 #include<fstream>
 using namespace std;
+
 int main()
 {
 	ifstream cin("input.txt"); //把输入流定位到aaa.txt文件
-	double nums[1000]; //你的数组
+	int nums[1000] = { 0 }; //数组
 	int m, n;//行列
 	cin >> m >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> nums[i];
 	}
+	cout << "数组的行为" << m << "数组的列为" << n << endl;;
+	cout << "数组的数据如下" << endl;
+		for (int i = 0; i < n; i++) {
+			cout << nums[i] << "    ";
+		}
 	int pre = 0, maxAns = nums[0];
 	for (const auto& x : nums) {
 		pre = max(pre + x, x);
 		maxAns = max(maxAns, pre);
 	}
-	cout <<"最大子数组的和为"<< maxAns;
+	cout << endl << "最大子数组的和为" << maxAns;
 }
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
